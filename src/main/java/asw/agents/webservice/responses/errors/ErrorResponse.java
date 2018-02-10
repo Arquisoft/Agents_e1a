d257537e017d6729fdc3f47027f0966e9045a67d
joaquin_ <uo250687@uniovi.es>
2018-02-10 22:34:50 +0100

    modificador public en constantes a ver si cambia el codacy ...

--- src/main/java/asw/agents/webservice/responses/errors/ErrorResponse.java
+++ src/main/java/asw/agents/webservice/responses/errors/ErrorResponse.java
@@ -2,18 +2,18 @@ package asw.agents.webservice.responses.errors;
 
 public abstract class ErrorResponse extends RuntimeException {
 
-	final static String KIND_IS_REQUIRED = "Kind is required";
-	final static String LOGIN_INCORRECT = "Login Incorrect";
-	final static String INCORRECT_PASSWORD = "Password Incorrect"; 
-	final static String KIND_IS_MISSING = "Internal error - Kind is missing in system master file";
-	final static String REQUIRED_USERNAME="The userName is required";
-	final static String REQUIRED_PASSWORD ="The password is required";
-	final static String USER_NOT_FOUND ="User not found";
-	final static String WRONG_EMAIL_STYLE="Wrong Email Style";
-	final static String INCORRECT_PASSWORD_DO_NOT_MATCH=" The passwords not match";
-	final static String SAME_EMAIL="same Email ";
-	final static String KIND_FILE_ERROR=" Kind file error ";
-	final static String UNKNOWN_ERROR=" Unknown error ";
+	public final static String KIND_IS_REQUIRED = "Kind is required";
+	public final static String LOGIN_INCORRECT = "Login Incorrect";
+	public final static String INCORRECT_PASSWORD = "Password Incorrect";
+	public final static String KIND_IS_MISSING = "Internal error - Kind is missing in system master file";
+	public final static String REQUIRED_USERNAME = "The userName is required";
+	public final static String REQUIRED_PASSWORD = "The password is required";
+	public final static String USER_NOT_FOUND = "User not found";
+	public final static String WRONG_EMAIL_STYLE = "Wrong Email Style";
+	public final static String INCORRECT_PASSWORD_DO_NOT_MATCH = " The passwords not match";
+	public final static String SAME_EMAIL = "same Email ";
+	public final static String KIND_FILE_ERROR = " Kind file error ";
+	public final static String UNKNOWN_ERROR = " Unknown error ";
 
 	private static final long serialVersionUID = 1L;
 
